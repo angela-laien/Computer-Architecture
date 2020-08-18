cac2cb29e23a534ea4fccf4a57e3882b6ad6d08c
Laien Xie <angela@Laiens-MacBook-Air.local>
2020-08-18 16:37:50 -0700

    still working on day 2

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -17,34 +17,52 @@ class CPU:
     def load(self):
         """Load a program into memory."""
 
-        address = 0
+        if len(sys.argv) < 2:
+            print("try another file")
+            sys.exit()
+
+        try:
+            address = 0
+            with open(sys.argv[1]) as f:
+                for line in f:
+                    comment_split = line.split("#")
+                    n = comment_split[0].strip()
+
+                    if n == '':
+                        continue
+
+                    self.ram[address] = int(n, 2)
+                    address += 1
+        except:
+            print("can not find it!")
+            sys.exit()
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        # for instruction in program:
+        #     self.ram[address] = instruction
+        #     address += 1
 
     def ram_read(self, MAR):
         # accept the address to read and return the value stored there
         # Memory Address Register (MAR): contains the address that is being read or written to
         return self.ram[MAR]
 
-    def raw_write(self, MAR, MDR):
+    def ram_write(self, MAR, MDR):
         # accept a value to write, and the address to write it to
         # Memory Data Register (MDR): contains the data that was read or the data to write
-        MDR = self.ram[MAR]
-        return MDR
+        self.ram[MAR] = MDR
+        # return MDR
 
 
     def alu(self, op, reg_a, reg_b):
@@ -53,33 +71,45 @@ class CPU:
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         #elif op == "SUB": etc
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         else:
             raise Exception("Unsupported ALU operation")
 
-    def trace(self):
-        """
-        Handy function to print out the CPU state. You might want to call this
-        from run() if you need help debugging.
-        """
+    # def trace(self):
+    #     """
+    #     Handy function to print out the CPU state. You might want to call this
+    #     from run() if you need help debugging.
+    #     """
 
-        print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
-            #self.fl,
-            #self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
-        ), end='')
+    #     print(f"TRACE: %02X | %02X %02X %02X |" % (
+    #         self.pc,
+    #         #self.fl,
+    #         #self.ie,
+    #         self.ram_read(self.pc),
+    #         self.ram_read(self.pc + 1),
+    #         self.ram_read(self.pc + 2)
+    #     ), end='')
 
-        for i in range(8):
-            print(" %02X" % self.reg[i], end='')
+    #     for i in range(8):
+    #         print(" %02X" % self.reg[i], end='')
 
-        print()
+    #     print()
+
+    def ldi(self, operand_a, operand_b):
+        # Set the value of a register to an integer.
+        self.reg[operand_a] = operand_b
+        
+    def prn(self, operand_a):
+        # Print numeric value stored in the given register
+        print(self.reg[operand_a])
 
     def run(self):
         """Run the CPU."""
-        # IR, the Instruction Register - memory address that’s stored in register PC
-        # Using ram_read(), read the bytes at PC+1 and PC+2 from RAM into variables operand_a and operand_b
+       
+        # Halt the CPU (and exit the emulator)
+        self.load()
+
         HLT = 0b00000001 
         LDI = 0b10000010 
         PRN = 0b01000111 
@@ -87,20 +117,23 @@ class CPU:
         running = True
 
         while running:
-            IR = self.ram[self.pc]
+            IR = self.ram_read(self.pc)
 
-            operand_a = self.ram[self.pc + 1]
-            operand_b = self.ram[self.pc + 2]
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
 
-        if IR == HLT:
-            running = False
-            self.pc += 1 
+            if IR == HLT:
+                running = False
+                # total number of bytes in any instruction is the number of operands + 1
+                self.pc += 1 
 
-        elif IR == LDI:
-            self.pc += 2 
+            elif IR == LDI:
+                self.ldi(operand_a, operand_b)
+                self.pc += 3
 
-        elif IR == PRN:
-            self.pc += 2
+            elif IR == PRN:
+                self.prn(operand_a)
+                self.pc += 2
 
-        else:
-            self.pc += 1 
+            else:
+                running = False
